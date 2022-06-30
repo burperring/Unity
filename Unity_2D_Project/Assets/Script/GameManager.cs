c0722367285aa84df386bed413aefbb75e767163
burperring <khw5231@gmail.com>
2022-06-30 16:47:28 +0900

    Unity_2D_Project
    Unity_2D_Project

--- Unity_2D_Project/Assets/Script/GameManager.cs
+++ Unity_2D_Project/Assets/Script/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,17 +10,99 @@ public class GameManager : MonoBehaviour
     public int stagePoint;
     public int stageIndex;
     public int health;
+    public Player_Move player;
+    public GameObject[] Stages;
+
+    // UI Value
+    public Image[] UIHealth;
+    public Text UIPoint;
+    public Text UIStage;
+    public GameObject UIRestart;
+
+    void Update()
+    {
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+    }
 
     public void NextStage()
     {
-        stageIndex++;
+        // Change Stage
+        if (stageIndex < Stages.Length -1)
+        {
+            Stages[stageIndex].SetActive(false);
+            stageIndex++;
+            Stages[stageIndex].SetActive(true);
+            PlayerReposition();
+
+            UIStage.text = "STAGE " + (stageIndex + 1);
+        }
+        else  // Game Clear
+        {
+            // Player Control Lock
+            Time.timeScale = 0;
+
+            // Result UI
+
+            // Restart Button UI
+            Text btnText = UIRestart.GetComponentInChildren<Text>();  // GetComponentInChildren : 해당 오브젝트의 자식 오브젝트에 접근해야 할 경우
+            btnText.text = "RESTART?";
+            UIStage.text = "GAME CLEAR!";
+            UIRestart.SetActive(true);
+        }
+
+        // Calculate Point
         totalPoint += stagePoint;
         stagePoint = 0;
+    }
+
+    public void HealthDown()
+    {
+        if (health > 1)
+        {
+            health--;
+            UIHealth[health].color = new Color(1, 1, 1, 0.2f);
+            player.PlaySound("DAMAGED");
+        }
+        else
+        {
+            // Health Down
+            UIHealth[0].color = new Color(1, 1, 1, 0.2f);
+            player.PlaySound("DAMAGED");
+
+            // Player Die Effect
+            player.PlayerDie();
+
+            // Result UI
+
+            // Retry Button UI
+            UIRestart.SetActive(true);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
-            health -= 3;
+        {
+            // Player Reposition
+            if (health > 1)
+            {
+                PlayerReposition();
+            }
+
+            // Health Down
+            HealthDown();
+        }
+    }
+
+    void PlayerReposition()
+    {
+        player.transform.position = new Vector3(0, 0, -1);
+        player.VelocityZero();
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
     }
 }
--- Unity_2D_Project/Assets/Script/Player_Move.cs
+++ Unity_2D_Project/Assets/Script/Player_Move.cs
@@ -5,17 +5,28 @@ using UnityEngine;
 public class Player_Move : MonoBehaviour
 {
     public GameManager gameManager;
+    public AudioClip audioJump;
+    public AudioClip audioAttack;
+    public AudioClip audioDamaged;
+    public AudioClip audioItem;
+    public AudioClip audioDie;
+    public AudioClip audioFinish;
     public float jumpPower;
     public float maxSpeed;
+
     Rigidbody2D rigid;
     SpriteRenderer sRender;
     Animator ani;
+    CapsuleCollider2D col;
+    AudioSource audioSource;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         sRender = GetComponent<SpriteRenderer>();
         ani = GetComponent<Animator>();
+        col = GetComponent<CapsuleCollider2D>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()   // 1회 입력을 해서 진행할 경우 그냥 Update 사용
@@ -25,6 +36,7 @@ public class Player_Move : MonoBehaviour
         {
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             ani.SetBool("isJumping", true);
+            PlaySound("JUMP");
         }
 
         // ReleaseKey By Stop Speed
@@ -83,9 +95,15 @@ public class Player_Move : MonoBehaviour
         {
             // Attack
             if (rigid.velocity.y < 0 && transform.position.y > collision.transform.position.y)
+            {
                 OnAttack(collision.transform);
+                PlaySound("ATTACK");
+            }
             else // Damaged
+            {
                 OnDamaged(collision.transform.position);
+                PlaySound("DAMAGED");
+            }
         }
     }
 
@@ -107,11 +125,13 @@ public class Player_Move : MonoBehaviour
 
             // Deactive Item
             collision.gameObject.SetActive(false);
+            PlaySound("ITEM");
         }
         else if(collision.gameObject.tag == "Finish")
         {
             // Next Stage
             gameManager.NextStage();
+            PlaySound("FINISH");
         }
     }
 
@@ -130,6 +150,9 @@ public class Player_Move : MonoBehaviour
 
     void OnDamaged(Vector2 targetPos)
     {
+        // Health Down
+        gameManager.HealthDown();
+
         // Animation
         ani.SetTrigger("doDamaged");
 
@@ -153,5 +176,52 @@ public class Player_Move : MonoBehaviour
 
         // View Alpha
         sRender.color = new Color(1, 1, 1, 1);
+    }
+
+    public void PlayerDie()
+    {
+        // Sprite Alpha
+        sRender.color = new Color(1, 1, 1, 0.4f);
+
+        // Sprite Flip Y
+        sRender.flipY = true;
+
+        // Collider Disable
+        col.enabled = false;
+
+        // Die Effect Jump
+        rigid.AddForce(Vector2.up * 2.5f, ForceMode2D.Impulse);
+    }
+
+    public void VelocityZero()
+    {
+        rigid.velocity = Vector2.zero;
+    }
+
+    public void PlaySound(string action)
+    {
+        switch (action)
+        {
+            case "JUMP":
+                audioSource.clip = audioJump;
+                break;
+            case "ATTACK":
+                audioSource.clip = audioAttack;
+                break;
+            case "DAMAGED":
+                audioSource.clip = audioDamaged;
+                break;
+            case "ITEM":
+                audioSource.clip = audioItem;
+                break;
+            case "DIE":
+                audioSource.clip = audioDie;
+                break;
+            case "FINISH":
+                audioSource.clip = audioFinish;
+                break;
+        }
+
+        audioSource.Play();
     }
 }
