e3b8f23b84173b167c573ee6e16ddddb7ddd799f
burperring <khw5231@gmail.com>
2022-07-05 15:57:38 +0900

    Unity_2D_ShootingGame
    Unity_2D_ShootingGame

---
+++ Unity_2D_ShootingGame/Assets/Scripts/Explanation.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explanation : MonoBehaviour
+{
+    // 1. 게임 화면 비율
+    //    1) 게임 창에서 Free Aspect의 경우 창 크기에 따른 화면 비율을 자동으로 맞춰주는 것이다.
+    //    2) 고정된 비율을 가지려면 해당 버튼을 눌러 원하는 비율을 세팅하면 된다.
+    //    3) 원하는 비율이 없을 경우 새롭게 만들면 된다. 이때 Aspect Ratio로 만들게 되면 비율만 고정되고 크기는 자유롭게 변한다.
+
+    // 2. Rigidbody 2D
+    //    1) Body Types를 Kinematics로 설정하게 된다면 물리연산을 안받게 된다.
+    //    2) Kinematics로 설정하면 Rigidbody 끼리의 충돌 물리연산도 안받게 된다.
+}
---
+++ Unity_2D_ShootingGame/Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public bool isTouchTop;
+    public bool isTouchBottom;
+    public bool isTouchLeft;
+    public bool isTouchRight;
+
+    Animator anim;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // Block Left Rignt Border
+        float h = Input.GetAxisRaw("Horizontal");
+        if ((isTouchRight && h == 1) || (isTouchLeft && h == -1))
+            h = 0;
+
+        // Block Top Bottom Border
+        float v = Input.GetAxisRaw("Vertical");
+        if ((isTouchTop && v == 1) || (isTouchBottom && v == -1))
+            v = 0;
+
+        // Player Move
+        Vector3 curPos = transform.position;
+        Vector3 nextPos = new Vector3(h, v, 0) * speed * Time.deltaTime;    // transform 이동에는 deltaTime 꼭 사용하기
+        transform.position = curPos + nextPos;
+
+        // Player Animation
+        if(Input.GetButtonDown("Horizontal") || Input.GetButtonUp("Horizontal"))
+        {
+            anim.SetInteger("Input", (int)h);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Border")
+        {
+            switch(collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = true;
+                    break;
+                case "Bottom":
+                    isTouchBottom = true;
+                    break;
+                case "Left":
+                    isTouchLeft = true;
+                    break;
+                case "Right":
+                    isTouchRight = true;
+                    break;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Border")
+        {
+            switch (collision.gameObject.name)
+            {
+                case "Top":
+                    isTouchTop = false;
+                    break;
+                case "Bottom":
+                    isTouchBottom = false;
+                    break;
+                case "Left":
+                    isTouchLeft = false;
+                    break;
+                case "Right":
+                    isTouchRight = false;
+                    break;
+            }
+        }
+    }
+}
