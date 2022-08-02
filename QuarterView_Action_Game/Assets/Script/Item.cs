6a2796c541a5c6d09a0e1657b641d89931883af4
burperring <khw5231@gmail.com>
2022-08-03 00:17:20 +0900

    QuarterView_Action_Game
    QuarterView_Action_Game

---
+++ QuarterView_Action_Game/Assets/Script/Item.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public enum Type { Ammo, Coin, Grenade, Heart, Weapon };
+    public Type type;
+    public int value;
+
+    private void Update()
+    {
+        // Item Rotate(È¸Àü)
+        transform.Rotate(Vector3.up * 30 * Time.deltaTime);
+    }
+}
--- QuarterView_Action_Game/Assets/Script/Player.cs
+++ QuarterView_Action_Game/Assets/Script/Player.cs
@@ -5,35 +5,198 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public GameObject[] weapons;
+    public bool[] hasWeapons;
 
     float hAxis;
     float vAxis;
+    int equipWeaponIndex = -1;
+
+    // Input Button Value
     bool wDown;
+    bool jDown;
+    bool xDown;
+    bool iDown;
+    bool sDown1;
+    bool sDown2;
+    bool sDown3;
 
+    // Check Player Action
+    bool isJump;
+    bool isDodge;
+    bool isSwap;
+
+    // Player Move Vec
     Vector3 moveVec;
+    Vector3 dodgeVec;
 
+    Rigidbody rigid;
     Animator anim;
+
+    GameObject nearObject;
+    GameObject equipWeapon;
 
     void Awake()
     {
+        rigid = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+        Jump();
+        Dodge();
+        Swap();
+        Interation();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+        jDown = Input.GetButtonDown("Jump");
+        xDown = Input.GetButtonDown("Dash");
+        iDown = Input.GetButtonDown("Interation");
+        sDown1 = Input.GetButtonDown("Swap1");
+        sDown2 = Input.GetButtonDown("Swap2");
+        sDown3 = Input.GetButtonDown("Swap3");
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+
+        if (isDodge)
+            moveVec = dodgeVec;
 
         // Rigidbody에서 Freeze Rotation X, Z를 true로 해줘야 캐릭터가 쓰러지지 않는다.
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
+    }
 
+    void Turn()
+    {
         // Player Loot
         transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if(jDown && !isJump && !isDodge && !isSwap)
+        {
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+        }
+    }
+
+    void Dodge()
+    {
+        if (xDown && !isJump && !isDodge && !isSwap && moveVec != Vector3.zero)
+        {
+            dodgeVec = moveVec;
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.5f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
+    void Swap()
+    {
+        // Check Player Get Weapon or Equip Weapon
+        if (sDown1 && (!hasWeapons[0] || equipWeaponIndex == 0))
+            return;
+        if (sDown2 && (!hasWeapons[1] || equipWeaponIndex == 1))
+            return;
+        if (sDown3 && (!hasWeapons[2] || equipWeaponIndex == 2))
+            return;
+
+        int weaponIndex = -1;
+        if (sDown1) weaponIndex = 0;
+        if (sDown2) weaponIndex = 1;
+        if (sDown3) weaponIndex = 2;
+
+        // Weapon Change
+        if ((sDown1 && !isJump && !isDodge && !isSwap) 
+            || (sDown2 && !isJump && !isDodge && !isSwap) 
+            || (sDown3 && !isJump && !isDodge && !isSwap))
+        {
+            if (equipWeapon != null)
+                equipWeapon.SetActive(false);
+
+            equipWeaponIndex = weaponIndex;
+            equipWeapon = weapons[weaponIndex];
+            equipWeapon.SetActive(true);
+
+            anim.SetTrigger("doSwap");
+
+            isSwap = true;
+
+            Invoke("SwapOut", 0.4f);
+        }
+    }
+
+    void SwapOut()
+    {
+        isSwap = false;
+    }
+
+    void Interation()
+    {
+        if(iDown && nearObject != null && !isJump && !isDodge)
+        {
+            if(nearObject.tag == "Weapon")
+            {
+                Item item = nearObject.GetComponent<Item>();
+                int weaponIndex = item.value;
+                hasWeapons[weaponIndex] = true;
+
+                // EquipWeapon
+                if (equipWeapon != null)
+                    equipWeapon.SetActive(false);
+
+                equipWeaponIndex = weaponIndex;
+                equipWeapon = weapons[weaponIndex];
+                equipWeapon.SetActive(true);
+
+                Destroy(nearObject);
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            anim.SetBool("isJump", false);
+            isJump = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Weapon")
+            nearObject = other.gameObject;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Weapon")
+            nearObject = null;
     }
 }
