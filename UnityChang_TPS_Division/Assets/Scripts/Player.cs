030481657050a62ea0624a565515311e65f78b5d
burperring <khw5231@gmail.com>
2022-08-13 00:53:00 +0900

    UnitiChang_TPS_Division
    UnitiChang_TPS_Division

--- UnityChang_TPS_Division/Assets/Scripts/Player.cs
+++ UnityChang_TPS_Division/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Player Status Setting
+    public float donwSpeed;
     public float walkSpeed;
     public float runSpeed;
     public float jumpPower;
@@ -14,11 +15,16 @@ public class Player : MonoBehaviour
     private float vAxis;        // Vertical
     private bool runDown;       // Run : Left Shift
     private bool jumpDown;      // Jump : Spacebar
+    private bool getDown;       // DownUp : Ctrl
+    private bool sDown1;        // Weapon1 : 1
+    private bool sDown2;        // Weapon2 : 2
+    private bool sDown3;        // Hand : 3
 
     // Check Player Action
-    float jumpDelay = 0.8f;
-    float jumpTime = 0.8f;
+    float jumpDelay = 0.9f;
+    float jumpTime = 0.9f;
     bool isMove;
+    bool isDown;
 
     Vector3 moveVec;
 
@@ -39,6 +45,8 @@ public class Player : MonoBehaviour
         MoveCheck();
         Move();
         Jump();
+        Swap();
+        UpDown();
     }
 
     void GetInput()
@@ -47,6 +55,10 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical");
         runDown = Input.GetButton("Run");
         jumpDown = Input.GetButtonDown("Jump");
+        getDown = Input.GetButtonDown("DownUp");
+        sDown1 = Input.GetButtonDown("Weapon1");
+        sDown2 = Input.GetButtonDown("Weapon2");
+        sDown3 = Input.GetButtonDown("Weapon3");
     }
 
     void MoveCheck()
@@ -59,22 +71,56 @@ public class Player : MonoBehaviour
 
     void Move()
     {
+        if (isDown && vAxis == 0)
+        {
+            anim.SetFloat("Vertical", vAxis);
+            return;
+        }
+
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVec * (runDown ? runSpeed : walkSpeed) * Time.deltaTime;
+        transform.position += moveVec * (isDown ? donwSpeed : runDown ? runSpeed : walkSpeed) * Time.deltaTime;
 
         anim.SetFloat("Horizontal", hAxis);
         anim.SetFloat("Vertical", vAxis);
-        anim.SetFloat("Speed", isMove ? runDown ? runSpeed : walkSpeed : 0);
+        anim.SetFloat("Speed", isMove ? isDown ? donwSpeed : runDown ? runSpeed : walkSpeed : 0);
     }
 
     void Jump()
     {
-        if (jumpDown && jumpTime > jumpDelay)
+        if (!isDown && jumpDown && jumpTime > jumpDelay)
         {
             rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
             anim.SetTrigger("doJump");
             jumpTime = 0f;
+        }
+    }
+
+    void Swap()
+    {
+        if (sDown1 || sDown2)
+        {
+            anim.SetBool("isStrafing", true);
+            jumpDelay = 1.5f;
+        }
+        else if (sDown3)
+        {
+            anim.SetBool("isStrafing", false);
+            jumpDelay = 0.9f;
+        }
+    }
+
+    void UpDown()
+    {
+        if (getDown && !isDown)
+        {
+            anim.SetBool("isDown", true);
+            isDown = true;
+        }
+        else if (getDown && isDown)
+        {
+            anim.SetBool("isDown", false);
+            isDown = false;
         }
     }
 }
