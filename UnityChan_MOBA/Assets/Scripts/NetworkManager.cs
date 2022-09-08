68e45f29d93d7c704c6fe772fd2916c1b3f7aeea
burperring <khw5231@gmail.com>
2022-09-09 00:53:11 +0900

    Photon Network RPC setting
    Photon Network RPC setting

--- UnityChan_MOBA/Assets/Scripts/NetworkManager.cs
+++ UnityChan_MOBA/Assets/Scripts/NetworkManager.cs
@@ -27,17 +27,32 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     [Header("RoomPanel")]
     public GameObject roomPanel;
+    public GameObject gameStartBtn;
     public TMP_Text listText;
     public TMP_Text roomInfoText;
+    public TMP_Text clientNameText;
+    public TMP_Text nonClientNameText;
     public TMP_Text[] chatText;
     public TMP_InputField chatInput;
+    public Image clientCharImg;
+    public Image nonClientCharImg;
+
+    [Header("CharacterSelectPanel")]
+    public GameObject chSelectPanel;
 
     [Header("ETC")]
     public TMP_Text statusText;
     public PhotonView PV;
 
+    [Header("SetValue")]
+    public Sprite[] CharacterImg;
+    public int iClientSelectChar;
+    public int iNonClientSelectChar;
+
     List<RoomInfo> myList = new List<RoomInfo>();
     int currentPage = 1, maxPage, multiple;
+
+    Hashtable SC;
 
     #region Roomlist update
     // Pervious : -2, Next : -1
@@ -101,7 +116,11 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public void Connect() => PhotonNetwork.ConnectUsingSettings();
 
-    public override void OnConnectedToMaster() => PhotonNetwork.JoinLobby();
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
 
     public override void OnJoinedLobby()
     {
@@ -135,6 +154,11 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         disConnectPanel.SetActive(false);
         lobbyPanel.SetActive(false);
         roomPanel.SetActive(true);
+
+        gameStartBtn.SetActive(PhotonNetwork.IsMasterClient);
+
+        PhotonNetwork.LocalPlayer.SetCustomProperties(new Hashtable { { "SelectCharacter", 5 }, { "IsReady", false } });
+
         RoomRenewal();
         chatInput.text = "";
 
@@ -142,6 +166,11 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         {
             chatText[i].text = "";
         }
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        gameStartBtn.SetActive(PhotonNetwork.IsMasterClient);
     }
 
     public override void OnCreateRoomFailed(short returnCode, string message)
@@ -170,14 +199,84 @@ public class NetworkManager : MonoBehaviourPunCallbacks
 
     public void RoomRenewal()
     {
-        listText.text = "";
+        int setNum;
 
-        for(int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        SetDefault();
+
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
         {
             listText.text += PhotonNetwork.PlayerList[i].NickName + ((i + 1 == PhotonNetwork.PlayerList.Length) ? "" : ", ");
         }
 
+        PV.RPC("SetNicnameRPC", RpcTarget.All, PhotonNetwork.LocalPlayer.NickName, PhotonNetwork.IsMasterClient);
+
+        for (int i = 0; i < PhotonNetwork.PlayerList.Length; i++)
+        {
+            PV.RPC("SetCharacterPRC", RpcTarget.All, setNum = PhotonNetwork.IsMasterClient ? iClientSelectChar : iNonClientSelectChar, PhotonNetwork.IsMasterClient);
+        }
+
         roomInfoText.text = PhotonNetwork.CurrentRoom.Name + " / " + PhotonNetwork.CurrentRoom.PlayerCount + "명 / 최대 " + PhotonNetwork.CurrentRoom.MaxPlayers + "명";
+    }
+
+    public void SetDefault()
+    {
+        listText.text = "";
+        clientNameText.text = "";
+        nonClientNameText.text = "";
+        clientCharImg.sprite = CharacterImg[5];
+        nonClientCharImg.sprite = CharacterImg[5];
+        iClientSelectChar = 5;
+        iNonClientSelectChar = 5;
+    }
+
+    [PunRPC]
+    public void SetNicnameRPC(string msg, bool isClient)
+    {
+        if (isClient)
+            clientNameText.text = msg;
+        else
+            nonClientNameText.text = msg;
+    }
+
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public void CharacterPanelOpen()
+    {
+        chSelectPanel.SetActive(true);
+    }
+    #endregion
+
+    #region Character select
+    public void CloseCharacterPanel()
+    {
+        chSelectPanel.SetActive(false);
+    }
+
+    public void SelectCharacter(int num)
+    {
+        // SC : SelectCharacter, IsReady
+        SC = PhotonNetwork.LocalPlayer.CustomProperties;
+
+        SC["SelectCharacter"] = num;
+
+        if (PhotonNetwork.IsMasterClient)
+            iClientSelectChar = num;
+        else
+            iNonClientSelectChar = num;
+
+        PV.RPC("SetCharacterPRC", RpcTarget.All, SC["SelectCharacter"], PhotonNetwork.IsMasterClient);
+    }
+
+    [PunRPC]
+    public void SetCharacterPRC(int num, bool isClient)
+    {
+        if (isClient)
+            clientCharImg.sprite = CharacterImg[num];
+        else
+            nonClientCharImg.sprite = CharacterImg[num];
     }
     #endregion
 
